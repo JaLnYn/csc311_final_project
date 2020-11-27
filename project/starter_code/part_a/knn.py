ca25f38da58f163c505646965e556d89b26d6ab7
JaLnYn <alan.yuan.jly@gmail.com>
2020-11-26 19:58:07 -0500

    completed question 1

--- project/starter_code/part_a/knn.py
+++ project/starter_code/part_a/knn.py
@@ -1,5 +1,6 @@
 from sklearn.impute import KNNImputer
 from utils import *
+import matplotlib.pyplot as plt
 
 
 def knn_impute_by_user(matrix, valid_data, k):
@@ -37,7 +38,11 @@ def knn_impute_by_item(matrix, valid_data, k):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    acc = None
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix.T)
+    acc = sparse_matrix_evaluate(valid_data, mat.T)
+    print("Validation Accuracy: {}".format(acc))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -60,7 +65,30 @@ def main():
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+    k_values = [1,6,11,16,21,26]
+    k_performance_user = [None] * 6
+    k_star_user = 0
+    print("by users")
+    for i in range(len(k_values)):
+        k_performance_user[i] = knn_impute_by_user(sparse_matrix, val_data, k_values[i])
+        if k_performance_user[k_star_user] < k_performance_user[i]:
+            k_star_user = i
+    print("We find that the best value for k, k* is", k_values[k_star_user] )
+    plt.plot(k_values,k_performance_user, label = 'user')
+    plt.legend()
+    plt.savefig('../figs/knn_user')
+    plt.cla()
+    print("by questions")
+    k_performance_item = [None] * 6
+    k_star_item = 0
+    for i in range(len(k_values)):
+        k_performance_item[i] = knn_impute_by_item(sparse_matrix, val_data, k_values[i])
+        if k_performance_item[k_star_item] < k_performance_item[i]:
+            k_star_item = i
+    print("We find that the best value for k, k* is", k_values[k_star_item] )
+    plt.plot(k_values,k_performance_item, label = 'item')
+    plt.legend()
+    plt.savefig('../figs/knn_item')
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
