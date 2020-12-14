185f756382b5a2639b0a0f7e4212b45b7334d625
Alan <alan.yuan.jly@gmail.com>
2020-12-14 00:10:11 +0000

    trained some models

--- project/starter_code/part_a/mf3.py
+++ project/starter_code/part_a/mf3.py
@@ -442,10 +442,10 @@ def main():
     run_sgd = 1
     run_nn = 1
     nn_load = 1
-    shoud_sgd_load = 0
+    shoud_sgd_load = 1
     generate_priv = 0
     nn_model_path = "./models/nn"
-    sgd_model_path = "./models/sgd_k30"
+    sgd_model_path = "./models/sgd_k60"
     #np.save(sgd_model_path, sgd_matrix)   
     nn_model = None
     sgd_matrix = None
@@ -466,7 +466,7 @@ def main():
     if shoud_sgd_load == 1:
         sgd_matrix = sgd_load(sgd_model_path)
     elif run_sgd == 1:
-        k_value = 50
+        k_value = 60
         # prev 2000000
         # prev 1250000
         #sgd_matrix = als(bootstrap(train_data, int(len(train_data["is_correct"])*3/4)),k_value,0.01, 1000000, 0.065)
